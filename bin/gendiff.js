dd21dd64d435b967e874fbb124af8295a27a91f3
Armine Palanjyan <a.p.t2@mail.ru>
2026-02-22 14:54:55 +0400

    added arguments and options in CLI command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,8 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
+    .argument('<filepath1>', 'путь до первого файла')
+    .argument('<filepath2>', 'путь до второго файла')
+    .option('-f, --format <type>', 'output format')
 
 program.parse()
