c572e7776dca98ad6843ce4fc9d4ae3421fd8d1f
Armine Palanjyan <a.p.t2@mail.ru>
2026-03-01 06:13:59 +0400

    added tests

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -14,8 +14,11 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const resultStylish = fs.readFileSync(getFixturePath('result.txt'), 'utf-8')
 const resultPlain = fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8')
 test('gendiff', () => {
-    expect(diff(getFixturePath('file1.json'), getFixturePath('file2.json') )).toBe(resultStylish)
-    expect(diff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml') )).toBe(resultStylish)
-    expect(diff(getFixturePath('file1.json'), getFixturePath('file2.json') )).toBe(resultPlain)
-    expect(diff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'))).toBe(resultPlain)
+    expect(diff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish' )).toBe(resultStylish)
+    expect(diff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml') , 'stylish')).toBe(resultStylish)
+    expect(diff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain' )).toBe(resultPlain)
+    expect(diff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'), 'plain')).toBe(resultPlain)
+
+    expect(diff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(resultStylish)
+    expect(diff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'))).toBe(resultStylish)
 })
